3200745ff850abd184508716991a47545eb798a0
Johan Mabille <johan.mabille@gmail.com>
2024-01-09 09:42:30 +0100

    Release 0.3.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,7 @@ cmake -DCMAKE_BUILD_TYPE=Release     \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX    \
       -DCMAKE_INSTALL_LIBDIR=lib     \
+      -DXEUS_CPP_BUILD_TESTS=OFF     \
       $SRC_DIR
 
 make install
